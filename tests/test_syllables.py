57313b68ae28a0045607deabd5da80d463f5db6b
David L. Day <dday376@gmail.com>
2018-11-25 20:33:35 -0500

    Added unit test for syllables.estimate()

--- tests/test_syllables.py
+++ tests/test_syllables.py
@@ -2,11 +2,10 @@
 import re
 
 import cmudict
+import syllables
 
-# Rapid test of syllable estimations
-# Will eventually become a unit test.
-
-def main():
+def test_estimate():
+    EXPECTED_ACCURACY = .75
     hits = []
     misses = []
 
@@ -16,210 +15,17 @@ def main():
         cmudict_syllables = 0
         for phone in phones:
             if re.match(r"\w*[012]$", phone):
-                cmudict_syllables += 1 
-        estimated_syllables = estimate_syllables(word)
+                cmudict_syllables += 1
+        estimated_syllables = syllables.estimate(word)
         if cmudict_syllables == estimated_syllables:
             hits.append(word)
         else:
             misses.append(word)
-            # print(word)
 
     hit = len(hits)
     miss = len(misses)
     total = hit + miss
-    print("Hits: {0}, Miss: {1}, Accuracy: {2}".format(hit, miss, hit / total))
-
-# Estimation stuff here
-
-subSyllables = [
-    'cial',
-    'tia',
-    'cius',
-    'cious',
-    'uiet',
-    'gious',
-    'geous',
-    'priest',
-    'giu',
-    'dge',
-    'ion',
-    'iou',
-    'sia$',
-    '.che$',
-    '.ched$',
-    '.abe$',
-    '.ace$',
-    '.ade$',
-    '.age$',
-    '.aged$',
-    '.ake$',
-    '.ale$',
-    '.aled$',
-    '.ales$',
-    '.ane$',
-    '.ame$',
-    '.ape$',
-    '.are$',
-    '.ase$',
-    '.ashed$',
-    '.asque$',
-    '.ate$',
-    '.ave$',
-    '.azed$',
-    '.awe$',
-    '.aze$',
-    '.aped$',
-    '.athe$',
-    '.athes$',
-    '.ece$',
-    '.ese$',
-    '.esque$',
-    '.esques$',
-    '.eze$',
-    '.gue$',
-    '.ibe$',
-    '.ice$',
-    '.ide$',
-    '.ife$',
-    '.ike$',
-    '.ile$',
-    '.ime$',
-    '.ine$',
-    '.ipe$',
-    '.iped$',
-    '.ire$',
-    '.ise$',
-    '.ished$',
-    '.ite$',
-    '.ive$',
-    '.ize$',
-    '.obe$',
-    '.ode$',
-    '.oke$',
-    '.ole$',
-    '.ome$',
-    '.one$',
-    '.ope$',
-    '.oque$',
-    '.ore$',
-    '.ose$',
-    '.osque$',
-    '.osques$',
-    '.ote$',
-    '.ove$',
-    '.pped$',
-    '.sse$',
-    '.ssed$',
-    '.ste$',
-    '.ube$',
-    '.uce$',
-    '.ude$',
-    '.uge$',
-    '.uke$',
-    '.ule$',
-    '.ules$',
-    '.uled$',
-    '.ume$',
-    '.une$',
-    '.upe$',
-    '.ure$',
-    '.use$',
-    '.ushed$',
-    '.ute$',
-    '.ved$',
-    '.we$',
-    '.wes$',
-    '.wed$',
-    '.yse$',
-    '.yze$',
-    '.rse$',
-    '.red$',
-    '.rce$',
-    '.rde$',
-    '.ily$',
-    '.ely$',
-    '.des$',
-    '.gged$',
-    '.kes$',
-    '.ced$',
-    '.ked$',
-    '.med$',
-    '.mes$',
-    '.ned$',
-    '.[sz]ed$',
-    '.nce$',
-    '.rles$',
-    '.nes$',
-    '.pes$',
-    '.tes$',
-    '.res$',
-    '.ves$',
-    'ere$'
-]
-
-addSyllables = [
-    'ia',
-    'riet',
-    'dien',
-    'ien',
-    'iet',
-    'iu',
-    'iest',
-    'io',
-    'ii',
-    'ily',
-    '.oala$',
-    '.iara$',
-    '.ying$',
-    '.earest',
-    '.arer',
-    '.aress',
-    '.eate$',
-    '.eation$',
-    '[aeiouym]bl$',
-    '[aeiou]{3}',
-    '^mc', 'ism',
-    '^mc', 'asm',
-    '([^aeiouy])1l$',
-    '[^l]lien',
-    '^coa[dglx].',
-    '[^gq]ua[^auieo]',
-    'dnt$'
-]
-
-re_subsyllables = []
-for s in subSyllables:
-    re_subsyllables.append(re.compile(s))
-
-re_addsyllables = []
-for s in addSyllables:
-    re_addsyllables.append(re.compile(s))
-
-
-def estimate_syllables(word):
-    parts = re.split(r'[^aeiouy]+', word)
-    valid_parts = []
-
-    for part in parts:
-        if part != '':
-            valid_parts.append(part)
-
-    syllables = 0
-
-    for p in re_subsyllables:
-        if p.match(word):
-            syllables -= 1
-
-    for p in re_addsyllables:
-        if p.match(word):
-            syllables += 1
-
-    syllables += len(valid_parts)
-
-    if syllables <= 0:
-        syllables = 1
-
-    return syllables
-
-if __name__ == "__main__":
-    main()
+    ACCURACY = hit / total
+    if (ACCURACY < EXPECTED_ACCURACY):
+        raise AssertionError('syllables.estimate(): Expected accuracy of {0}, got {1}.'.format(
+            EXPECTED_ACCURACY, ACCURACY))
