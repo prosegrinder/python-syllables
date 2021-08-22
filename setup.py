4f02b2749ecb9cbd88a1c90f05c0786ba0157ca4
David L. Day <dday376@gmail.com>
2021-08-22 10:56:59 -0400

    migrating settings to setup.cfg

--- setup.py
+++ setup.py
@@ -13,15 +13,16 @@ with open(path.join(path.dirname(__file__), 'README.md')) as readme_file:
 
 
 setup(
-    name="syllables",
-    version=VERSION,
-    url="https://github.com/prosegrinder/python-syllables",
+    # name="syllables",
+    # version=VERSION,
+    # url="https://github.com/prosegrinder/python-syllables",
 
-    author="David L. Day",
-    author_email="dday376@gmail.com",
+    # author="David L. Day",
+    # author_email="dday376@gmail.com",
 
-    description="A Python package for estimating the number of syllables in a word.",
-    long_description=LONG_DESCRIPTION,
+    # description="A Python package for estimating the number of syllables in a word.",
+    # long_description=LONG_DESCRIPTION,
+    # Description-Content-Type: text/markdown; charset=UTF-8; variant=GFM
 
     packages=[
         'syllables'
@@ -32,17 +33,17 @@ setup(
     },
     include_package_data=True,
 
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Natural Language :: English',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
+    # classifiers=[
+    #     'Development Status :: 4 - Beta',
+    #     'Intended Audience :: Developers',
+    #     'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    #     'Natural Language :: English',
+    #     'Programming Language :: Python',
+    #     'Programming Language :: Python :: 2',
+    #     'Programming Language :: Python :: 2.7',
+    #     'Programming Language :: Python :: 3',
+    #     'Programming Language :: Python :: 3.4',
+    #     'Programming Language :: Python :: 3.5',
+    #     'Programming Language :: Python :: 3.6',
+    # ],
 )
